a8c36fc7cf491f423cfa29024d0e61b18b6bdba7
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2026-05-13 13:04:26 +0200

    [20] - add default value to configuration

--- NotificationsService/Configuration/KafkaConfig.cs
+++ NotificationsService/Configuration/KafkaConfig.cs
@@ -3,7 +3,7 @@ namespace NotificationsService.Configuration;
 public class KafkaConfig
 {
     public const string SectionName = "Kafka";
-    public string Brokers { get; set; }
+    public string Brokers { get; set; } = "broker:29092";
     public string MotionTopic { get; set; } = "motion-events";
     public string GroupId { get; set; } = "notifications-group";
 }
